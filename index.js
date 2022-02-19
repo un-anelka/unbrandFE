f328ac3119062fa72ac6cdcd26c2e4e032142753
UN Anelka <un.rudasingwa@gmail.com>
2022-02-20 01:26:56 +0200

    Added dashboard and responsiveness of the landing page

---
+++ index.js
@@ -0,0 +1,23 @@
+
+  // menu bars/icon
+  function myFunction(x) {
+    x.classList.toggle("change");
+    navigation.classList.toggle("change");
+  }
+
+
+  function readMoreReadLess() {
+    var dots = document.getElementById("dots");
+    var moreText = document.getElementById("more");
+    var btnText = document.getElementById("myBtn");
+  
+    if (dots.style.display === "none") {
+      dots.style.display = "inline";
+      btnText.innerHTML = "Read more"; 
+      moreText.style.display = "none";
+    } else {
+      dots.style.display = "none";
+      btnText.innerHTML = "Read less"; 
+      moreText.style.display = "inline";
+    }
+  }
